f161cb89e37280f3cf4d628dd8c57466d0d6a1ca
mariambuzaladze <mariambuzaladze10@example.come>
2024-04-05 13:36:59 +0400

    feat: feedback component

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -3,9 +3,9 @@ import Hamburger from "/assets/shared/mobile/icon-hamburger.svg";
 import Close from "/assets/shared/mobile/icon-close.svg";
 import ArrowDown from "/assets/shared/icon-arrow-down.svg";
 import ArrowUp from "/assets/shared/icon-arrow-up.svg";
-import Comments from "/assets/shared/icon-comments.svg";
 import SortOption from './SortOption';
 import Category from './Category';
+import Feedback from './Feedback';
 
 export default function Feedbacks({ data, setData }) {
     const [showSortOptions, setShowSortOptions] = useState(false);
@@ -154,26 +154,7 @@ export default function Feedbacks({ data, setData }) {
                     .filter(feedback => selectedCategory === null || feedback.category === selectedCategory)
                     .map((feedback) => {
                         return (
-                            <div key={feedback.id} className="p-6 gap-3 rounded-2xl bg-white flex flex-col cursor-pointer">
-                                <p className="text-xs font-bold tracking-tighter text-blue-900">{feedback.title}</p>
-                                <p className="text-xs text-blue-gray-600">{feedback.description}</p>
-
-                                <div className="rounded-lg bg-slate-100 py-1 px-4 w-fit-content">
-                                    <p className="text-xs font-semibold text-blue-600">{feedback.category}</p>
-                                </div>
-
-                                <div className="flex justify-between">
-                                    <button className="flex gap-2 items-center rounded-lg bg-slate-100 py-1 px-4"  onClick={() => handleUpvote(feedback.id)}>
-                                        <img src={ArrowUp} alt="arrow up icon" />
-                                        <p className="text-xs font-bold tracking-tighter text-blue-900">{feedback.upvotes}</p>
-                                    </button>
-
-                                    <div className="flex items-center gap-1">
-                                        <img src={Comments} alt="comments icon" />
-                                        <p className="text-xs font-semibold text-blue-600">{feedback.comments?.length ?? 0}</p>
-                                    </div>
-                                </div>
-                            </div>
+                            <Feedback key={feedback.id} feedback={feedback} onClick={() => handleUpvote(feedback.id)}/>
                         );
                     })
                 }
