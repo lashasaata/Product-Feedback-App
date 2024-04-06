7dbc773adeba4c461840cc89b15107ae5f44f76b
mariambuzaladze <mariambuzaladze10@example.come>
2024-04-06 18:11:23 +0400

    fix: remake header

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -8,6 +8,7 @@ import SortOption from './SortOption';
 import Category from './Category';
 import Feedback from './Feedback';
 import EmptyFeedbacks from './EmptyFeedbacks';
+import Header from './Header';
 
 export default function Feedbacks({ data, setData }) {
     const [showSortOptions, setShowSortOptions] = useState(false);
@@ -66,11 +67,6 @@ export default function Feedbacks({ data, setData }) {
         }
     };
 
-    const toggleSidebar = () => {
-        setSidebarVisible(!sidebarVisible);
-        setOpacity(sidebarVisible ? 1 : 0.5);
-    };
-
     const getStatusCounts = () => {
         const statusCounts = [];
         data.productRequests.forEach(feedback => {
@@ -93,59 +89,7 @@ export default function Feedbacks({ data, setData }) {
 
     return (
         <div className="md:bg-slate-100 md:px-[39px] md:py-[56px] md:flex md:flex-col md:gap-6">
-            <header className='md:flex md:flex-col md:gap-[40px]'>
-                <div className="headerImg w-full flex justify-between items-center  py-4 px-6 md:bg-none  md:bg-slate-100">
-                    <div className="titleBackground md:w-[223px] md:h-[178px] md:p-6 rounded-lg md:flex md:flex-col md:justify-end">
-                        <p className="text-base font-bold tracking-tight text-white md:text-xl">Frontend Mentor</p>
-                        <p className="text-xs font-medium text-white opacity-75 md:text-base">Feedback Board</p>
-                    </div>
-
-                    <img className='cursor-pointer md:hidden' src={sidebarVisible? Close: Hamburger} alt="icon hamburger" onClick={toggleSidebar} />
-
-                    <div className='hidden bg-white rounded-lg md:flex flex-wrap items-start w-[200px] gap-2 p-6 '>
-                        <button className="p-1.5 md:p-2.5 lg:p-3 rounded-lg bg-blue-100 text-blue-600 text-sm md:text-base font-semibold" onClick={() => handleCategoryClick(null)}>All</button>
-                        {uniqueCategories.map((category,index)=>{
-                            return <Category key={index} onClick={() => handleCategoryClick(category)} theCategory={category}/>
-                        })}
-                    </div>
-
-                    <div className='hidden bg-white rounded-lg md:flex flex-col p-6 gap-6 rounded-lg'>
-                    <div className='flex justify-between items-center gap-4'>
-                        <p className="text-lg md:text-xl font-bold tracking-wide text-gray-700">Roadmap</p>
-                        <p className="text-xs md:text-xl font-bold tracking-wide text-blue-600 underline">View</p>
-                    </div>
-
-                    <div className='flex flex-col gap-2'>
-                        {getStatusCounts().map((status, id) => {
-                            return (
-                                status.status === "suggestion" ? null :
-                                <div className='flex justify-between' key={id} onClick={() => handleCategoryClick(status.status)}>
-                                    <p className="text-base text-gray-700">{status.status}</p>
-                                    <p className="text-base text-gray-700 font-bold">{status.count}</p>
-                                </div>
-                            );
-                        })}
-                    </div>
-                </div>
-                </div>
-
-                <div className="py-2 px-6 bg-sky-950 flex justify-between items-center md:rounded-lg" style={{ opacity: opacity }}>
-
-                    <div className='hidden md:flex items-center gap-3'>
-                        <img src={SuggestionImg} alt="ico suggestion" />
-                        <p className="text-white text-lg font-bold tracking-tighter">{sortedFeedbacks.length} Suggestions</p>
-                    </div>
-
-                    <div className="cursor-pointer flex items-center gap-2" onClick={() => setShowSortOptions(!showSortOptions)}>
-                        <p className="text-xs text-white font-bold"><span className="font-normal opacity-75">Sort by : </span>{sortBy === 'mostUpvotes' ? 'Most Upvotes' : sortBy === 'leastUpvotes' ? 'Least Upvotes' : sortBy === 'mostComments' ? 'Most Comments' : 'Least Comments'}</p>
-                        <img src={showSortOptions?ArrowUp:ArrowDown} alt="arrow down icon"/>
-                    </div>
-
-                    <div className="px-4 py-3 rounded-lg bg-purple-600">
-                        <button className="text-xs font-bold text-white">+ Add Feedback</button>
-                    </div>
-                </div>
-            </header>
+            <Header sidebarVisible={sidebarVisible} setSidebarVisible={setSidebarVisible} opacity={opacity} setOpacity={setOpacity} uniqueCategories={uniqueCategories} getStatusCounts={getStatusCounts} sortedFeedbacks={sortedFeedbacks} sortBy={sortBy} showSortOptions={showSortOptions} setShowSortOptions={setShowSortOptions} handleCategoryClick={handleCategoryClick}/>
 
             {showSortOptions && (
                 <div className="absolute rounded-lg bg-white cursor-pointer left-[58px] top-[112px] md:left-[44%] md:top-[371px]" style={{boxShadow: '0 10px 40px -7px rgba(55, 63, 104, 0.35)'  }}>
